dbc1bea64e22fd26d887344e8a07c259aac76fa9
Ephalys <ephalys@live.fr>
2021-02-06 00:11:53 +0100

    add boxsizing borderbox

--- src/components/layout/layoutStyles.js
+++ src/components/layout/layoutStyles.js
@@ -2,6 +2,10 @@ import { createGlobalStyle } from "styled-components"
 import colors from "styles/colors"
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     background-color: ${colors.black};
     color: ${colors.white};
