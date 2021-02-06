7be88eb2a49a18c666af205b6f79ab9ca296f341
Ephalys <ephalys@live.fr>
2021-02-06 22:11:56 +0100

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import * as React from "react"
 import Layout from "components/layout/layout";
+import Helmet from "react-helmet";
 
 const IndexPage = () => {
-  return (
-    <Layout/>
-  )
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Léon Tran-Van — Front-end developer</title>
+                <link rel="canonical" href="http://nvnrtnl.vercel.app" />
+                <meta name="description" content="The work of front-end developer Léon Tran-Van." />
+            </Helmet>
+            <Layout/>
+        </>
+    )
 }
 
 export default IndexPage
