7f2c53f961a061ac8cfbcdf426fa23f450b6f209
Ephalys <ephalys@live.fr>
2021-02-17 14:08:49 +0100

    fix billiards

--- src/components/textCentered/textCentered.js
+++ src/components/textCentered/textCentered.js
@@ -4,7 +4,7 @@ import {MeContainer} from "components/textCentered/textCenteredStyles";
 const TextCentered = () => {
     return (
         <MeContainer className="container">
-            In my spare time, I enjoy playing some video games, go for a pool, going to the gym and do motorcycle rides with friends.
+            In my spare time, I enjoy playing some video games, go play billiards, going to the gym and do motorcycle rides with friends.
         </MeContainer>
     );
 };
